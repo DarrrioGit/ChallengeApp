0aa26dfb26530c4a63aa54d23798ef1b47826582
DarrrioGit <matyjaszczyk.darek@gmail.com>
2023-02-18 17:40:37 +0100

    dzień 4

--- ChellengeApp/ChellengeApp/Program.cs
+++ ChellengeApp/ChellengeApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "famele";
+var age = 17;
+var age2 = 33;
+
+if (sex == "famele" && age < 30)
+{
+    //Console.WriteLine ("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+
+if (name == "Ewa" && age2 == 33)
+{
+    //Console.WriteLine ("Ewa lat 33");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
+
+if (!(sex == "famele" && age > 17))
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
